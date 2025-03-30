97e2e0cc237289c4cb33bca08823cf4be145f269
PS22059 <hungyasuo2016@gmail.com>
2025-03-31 01:11:02 +0700

    test

---
+++ src/app/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+export async function generateStaticParams() {
+  // Trả về một mảng các ID mà bạn muốn pre-render
+  return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    // Thêm các ID khác nếu cần
+  ]
+} 
